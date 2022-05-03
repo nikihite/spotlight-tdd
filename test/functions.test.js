e41bc4585fcd47b9cb2821b17755c6561e49bfb5
Niki Hite <nikihite@comcast.net>
2022-05-03 11:30:46 -0700

    finished test 3

--- test/functions.test.js
+++ test/functions.test.js
@@ -19,10 +19,18 @@ test('this test should add exclamation points to a string', (expect) => {
     expect.equal(actual, expected);
 });
 
-test ('this test should take a number and mutiply it by 7', (expect) =>{
+test ('this test should take a number and mutiply it by 7', (expect) => {
     const expected = 21; 
 
     const actual = multiplyBySeven(3);
+
+    expect.equal(actual, expected);
+});
+
+test ('this test should take a number and multiply that number by 12 and then, divide the number by half', (expect) => {
+    const expected = 24;
+
+    const actual = multiplyBy12ThenHalve(4);
 
     expect.equal(actual, expected);
 });
