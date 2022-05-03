26f2780bbf12b3174a1b8b71390ed2387618a76e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-03 12:21:39 -0600

    question 2 and imports

--- functions.js
+++ functions.js
@@ -20,7 +20,8 @@ export function addExclamationPoints(string) {
     return `${string}!!!`;
 }
 
-// This function should take in a number and multiply it by seven
+// This function should take in a number 
+//and multiply it by seven
 
 /* 
 Input:
@@ -33,7 +34,7 @@ Output:
 */
 
 export function multiplyBySeven(num) {
-    return true;
+    return num * 7;
 }
 
 // This function should take in a number and multiply it by twelve then cut the result in half
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { addExclamationPoints, myFunction } from '../functions.js';
+import { addExclamationPoints, myFunction, multiplyBySeven, multiplyBy12ThenHalve, divideThenMultiply, returnAsAnArray, returnAsAString, makeLuckyGreeting, getSecondItem, getLastItem, renderDogLI, renderDogDiv, } from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -15,6 +15,14 @@ test('this test should add exclamation points to a string', (expect) => {
     const expected = 'hi niki!!!';
 
     const actual = addExclamationPoints('hi niki');
+
+    expect.equal(actual, expected);
+});
+
+test ('this test should take a number and mutiply it by 7', (expect) =>{
+    const expected = 21; 
+
+    const actual = multiplyBySeven(3);
 
     expect.equal(actual, expected);
 });
